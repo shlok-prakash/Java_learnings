8b23811196cd92fdc706a4f9535e68fc203cca54
shlok-prakash <pandeyshlok10@gmail.com>
2024-11-30 00:46:14 +0530

    Commit

---
+++ src/ReverseOnlyLetters.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ReverseOnlyLetters {
+    public static void main(String[] args) {
+        System.out.print("Enter a String : ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        char[] inputArray = input.toCharArray();
+        int left =0;
+        int right = inputArray.length-1;
+        while(left<right){
+            if(!Character.isLetter(inputArray[left])){
+                left++;
+            }
+            else if (!Character.isLetter(inputArray[right])) {
+                right--;
+            }
+            else{
+                char temp = inputArray[left];
+                inputArray[left] = inputArray[right];
+                inputArray[right] = temp;
+                left++; right--;
+            }
+        }
+        input  = String.valueOf(inputArray);
+        System.out.print("Reversed String is : " + input);
+    }
+}
+//(x$l$`ek]$intly$l$ysbzatm
+//(m$t$`az]$bsytl$y$lnikelx
+//(m$t$`az]$bsyly$l$tnikelx
