10b2c1eac4a56aad7862cb3951d5255654bf701e
shlok-prakash <pandeyshlok10@gmail.com>
2025-02-06 21:27:49 +0530

    2 Leet code questions

---
+++ src/SecondLargest.java
@@ -0,0 +1,8 @@
+public class SecondLargest {
+    public static void main(String[] args) {
+//        int minValue = Integer.MIN_VALUE;
+//        int maxValue = Integer.MAX_VALUE;
+//        System.out.println(minValue);
+//        System.out.println(maxValue);
+    }
+}
